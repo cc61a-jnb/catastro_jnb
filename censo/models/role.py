ec0e85660404a09f25c5e07867296391041221a3
Gabriel Gayan Fanta <gabrielgayan@gmail.com>
2011-04-12 18:31:25 -0300

    arreglo print de los roles

--- censo/models/role.py
+++ censo/models/role.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 class Role(models.Model):
     name = models.CharField(max_length = 100, default = '')
     
-    def __str__(self):
+    def __unicode__(self):
         return self.name
 
     class Meta:
