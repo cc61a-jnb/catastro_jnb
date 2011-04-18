d80c4614ca9496e9bcd4f936499713e288dd1c35
Gabriel Gayan Fanta <gabrielgayan@gmail.com>
2011-04-18 12:52:43 -0300

    *agrego template  estático, aun no integrado. *configuro urls.py para funcionar con contenido estático, recuerden descargar última versión del settings.py!

--- urls.py
+++ urls.py
@@ -5,6 +5,8 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    (r'^media/(?P<path>.*)$', 'django.views.static.serve',
+        {'document_root': settings.MEDIA_ROOT}),
     (r'^admin/', include(admin.site.urls)),
 )
 
