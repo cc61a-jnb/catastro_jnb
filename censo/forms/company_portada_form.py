d34e7099da71c1a06fd8cadb81e6c76579d157d4
AdminConXP <cc61a@ubuntu.(none)>
2011-05-02 10:46:50 +0200

    * Started fixing company's first page (No models touched)

--- censo/forms/company_portada_form.py
+++ censo/forms/company_portada_form.py
@@ -6,16 +6,36 @@ from censo.models import *
 from django.forms.extras.widgets import SelectDateWidget
 from datetime import date
 from . import BaseForm
+from censo.utils import render_fields_as_table, render_fields_as_list, split_list, combine_fields_errors
 
 class CompanyPortadaForm(BaseForm):
     # Redefine foundation_date to use a different widget and year range
     foundation_date = forms.DateField(widget=SelectDateWidget(years = xrange(1800, date.today().year + 1)), label='Fecha fundación')
 
-    class Meta:
-        model = Company
-        fields = (
-        	'address',
-        	'phone',
-        	'foundation_date',
-        )
+    # Display company number question
+    def render_company_number(self):
+        fields = [self['number']]
+        
+        return render_fields_as_list(fields, 'list_quantities')
+    
+    # Display company address questions    
+    def render_company_address(self):
+        fields = [self['address'], self['commune']]
+        
+        return render_fields_as_list(fields)
+    
+    # Display company other data questions    
+    def render_company_data(self):
+        fields = [self['phone'], self['foundation_date']]
+        
+        return render_fields_as_list(fields)
 
+    class Meta:
+       model = Company
+       fields = (
+           'number',
+           'address',
+           'commune',
+           'phone',
+           'foundation_date',
+       )
