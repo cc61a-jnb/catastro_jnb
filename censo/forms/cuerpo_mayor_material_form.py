8ad287492233fe7d49b76ea6cba439b1b2b3a37d
AdminConXP <cc61a@ubuntu.(none)>
2011-05-30 12:19:15 +0200

    * Small fixes on Mayor Material form * Don't know where did the migration come from

--- censo/forms/cuerpo_mayor_material_form.py
+++ censo/forms/cuerpo_mayor_material_form.py
@@ -9,15 +9,32 @@ from . import BaseForm
 
 class CuerpoMayorMaterialForm(BaseForm):
 
-    #def clean(self):
-    #    data = self.cleaned_data
-    #    
-    #    if data['denomination'] is None:
-    #        error_message = 'Este campo es requerido'
-    #        self._errors['denomination'] = self.error_class([error_message])
-    vehicle_year = forms.DateField(label='Año del Vehículo', widget=SelectDateWidget(years=range(1900, 2020)))
-    service_incorporation_date = forms.DateField(label='Fecha de incorporación', widget=SelectDateWidget(years=range(1900, 2020)))
-    last_oil_change_date = forms.DateField(label='Fecha último cambio de aceite', widget=SelectDateWidget(years=range(1900, 2020)))
+    def clean(self):
+        data = self.cleaned_data
+        
+        self.custom_errors = []
+        
+        #Denomination is a required field
+        if data['denomination'] == u'':
+            error_message = 'Este campo es requerido'
+            self._errors['denomination'] = self.error_class([error_message])
+        
+        #The kilometers during the oil change cannot be higher that the current quantity    
+        if data['oil_change_kilometraje'] > data ['kilometraje']:
+            error_message = 'El kilometraje del último cambio de aceite no puede ser mayor al kilometraje actual'
+            self._errors['kilometraje'] = self.error_class([error_message])
+            self._errors['oil_change_kilometraje'] = self.error_class([error_message])
+            self.custom_errors.append(error_message)
+        
+        # If any validation fails, raise error
+        if self.custom_errors:
+            raise forms.ValidationError(self.custom_errors)
+        
+        return self.cleaned_data
+    
+    vehicle_year = forms.DateField(label='Año del Vehículo', widget=SelectDateWidget(years=range(1900, 2020)), required=False)
+    service_incorporation_date = forms.DateField(label='Fecha de incorporación', widget=SelectDateWidget(years=range(1900, 2020)), required=False)
+    last_oil_change_date = forms.DateField(label='Fecha último cambio de aceite', widget=SelectDateWidget(years=range(1900, 2020)), required=False)
         
     
     # Display company selector
@@ -42,7 +59,7 @@ class CuerpoMayorMaterialForm(BaseForm):
     def render_kilometraje_horometraje_to_list(self):
         fields = self._field_range('kilometraje', 'horometraje')
         
-        return render_fields_as_list(fields)
+        return render_fields_as_list(fields, css_class_name='list_quantities')
         
     # Display caracteristics as a list
     def render_caracteristics_to_list(self):
